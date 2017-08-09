8c6e5d931b59ef93516959277060e036d6fb1f9d
juniorUsca <junior.usca@gmail.com>
2017-08-09 17:16:41 -0500

    Rest get and request

--- crawler.js
+++ crawler.js
@@ -1,49 +1,48 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
+var http = require('http');
 
 
-/*var pageToVisit = "http://www.arstechnica.com";
-console.log("Visiting page " + pageToVisit);
-request(pageToVisit, function(error, response, body) {
-   if(error) {
-     console.log("Error: " + error);
-   }
-   // Check status code (200 is HTTP OK)
-   console.log("Status code: " + response.statusCode);
-   if(response.statusCode === 200) {
-     // Parse the document body
-     var $ = cheerio.load(body);
-     console.log("Page title:  " + $('title').text());
-   }
-});
-*/
-
-
-var pageToVisit = "http://190.119.255.126/egasa/Formularios/Inicio.aspx";
-var results = [];
-console.log("Visiting page " + pageToVisit);
-
-request(pageToVisit, function(error, response, body) {
-   if(error) {
-     console.log("Error: " + error);
-   }
-   // Check status code (200 is HTTP OK)
-   console.log("Status code: " + response.statusCode);
-   if(response.statusCode === 200) {
-     // Parse the document body
-     var $ = cheerio.load(body);
-     console.log("Page title:  " + $('title').text());
-
-    $('#Centro table tbody tr').not( $('#Centro table tbody tr').first() ).each(function(i, elem) {
-      results[i] = [];
-      $(this).children('td').each(function(j, elem) {
-        results[i][j] = $(this).text();
-        console.log( $(this).text() );
+function requestEgasa(callback) {
+  var pageToVisit = "http://190.119.255.126/egasa/Formularios/Inicio.aspx";
+  var data = [];
+  var res = {};
+  console.log("Visiting page " + pageToVisit);
+  request(pageToVisit, function(error, response, body) {
+    if(error) {
+      console.log("Error: " + error);
+    }
+    // Check status code (200 is HTTP OK)
+    console.log("Status code: " + response.statusCode);
+    if(response.statusCode === 200) {
+      // Parse the document body
+      var $ = cheerio.load(body);
+      $('#Centro table tbody tr').not( $('#Centro table tbody tr').first() ).each(function(i, elem) {
+        data[i] = [];
+        $(this).children('td').each(function(j, elem) {
+          data[i][j] = $(this).text();
+        });
       });
-      console.log( '-------' );
-    });
-    console.log( results );
-   }
-});
+      res ['data'] = data;
+      callback(data);
+    }
+  });
+}
 
+http.createServer(function(req, res){
+
+  //console.log(req.url);
+  if( req.url == '/' ) {
+    requestEgasa(function(data){
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.write(JSON.stringify(data));
+      res.end();
+    });
+  } else {
+    res.writeHead(404,{'Content-Type':'text/html'});
+    res.end('404 error');
+  }
+
+}).listen(9090);
+console.log ("server on port 9090");
